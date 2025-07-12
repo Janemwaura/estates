e804304bed5b4daa67df2ef9418b70fed9caf782
Janemwaura <mwaura461@gmail.com>
2025-07-12 12:53:14 +0300

    Hero section complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import Header from "/home/janewmwaura/personalprojectportfolio/estateproject/RealEstate-web-starterpack/src/components/Header/Header.jsx";
+import Hero from "./components/Hero/Hero";
+import './App.css';
 function App() {
   return (
     <div className="App">
+      <div>
+        <div className="white-gradient" />
       <Header/>
+      <Hero/>
+      </div>
     </div>
   );
 }
